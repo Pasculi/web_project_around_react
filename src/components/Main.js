53d0faea896e02d1802f86b0de034727611fdf3f
Jorge R. Sepulveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-04-18 18:07:38 -0400

    feat:Implementación de la información de usuario

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import imageProfile from '../images/image__profile.jpg'
 import '../blocks/Main/Main.css'
 import {api} from '../components/utils/api.js'
@@ -10,25 +10,34 @@ export default function Main({onEditProfileClick, onAddPlaceClick, onEditAvatarC
   const [userDescription, setUserDescription] = useState('');
   const [userAvatar, setUserAvatar] = useState('');
 
-  
+  useEffect(() => {
+    api.getUserInfo()
+      .then(res => {
+        setUserName(res.name)
+        setUserDescription(res.about)
+        setUserAvatar(res.avatar)
+      })
+      .catch(err => {
+        console.war("Error: " + err);
+      });
+  },[])
 
-  console.log(api)
   return (
     <>
       <section className="profile">
         <div className="profile__data">
           <div className="profile__avatar">
-            <img className="profile__avatar-image" src={imageProfile}
+            <img className="profile__avatar-image" src={userAvatar}
               alt="Avatar" />
             <button className="profile__avatar-edit" onClick={onEditAvatarClick}></button>
             <div className="profile__overlay-avatar"></div>
           </div>
           <div className="profile__presentation">
             <div className="profile__name">
-              <h1 className="profile__author">Jacques Cousteau</h1>
+              <h1 className="profile__author">{userName}</h1>
               <button className="btn btn-profile profile__author-button-edit" onClick={onEditProfileClick} id="btn_profile"></button>
             </div>
-            <h2 className="profile__activit">Explorador</h2>
+            <h2 className="profile__activit">{userDescription}</h2>
           </div>
         </div>
         <button className="btn bt-place profile__author-button-add-place" onClick={onAddPlaceClick} id="btn_place"></button>
